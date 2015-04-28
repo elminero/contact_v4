7eaca9005cc067eaaff028e8638e8732ed2137b8
Robert Farber <elminero@cox.net>
2015-04-28 21:22:36 +0000

    Set up db connection, allowed Vagrant IP for gii

---
+++ frontend/config/main-local.php
@@ -0,0 +1,37 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'KLOihn_il6Ek_5-6uGw4BSSmhV6qygWs',
+        ],
+    ],
+];
+
+/*
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+*/
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii']=[
+        'class' =>  'yii\gii\Module',
+        'allowedIPs' => ['*'],
+    ];
+}
+
+
+
+return $config;
