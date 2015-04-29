4ad5f61bfdb3fb578771c3e8a1755b1eaf4a8a66
Robert Farber <elminero@cox.net>
2015-04-29 00:37:45 +0000

    made site index more meaningfull

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -26,7 +26,7 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'My Company',
+                'brandLabel' => 'Contacts Version 4',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -5,15 +5,18 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Contacts Version 4</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">Keep Track of Your Contacts</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p>
+            <!--<a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a>-->
+        </p>
     </div>
 
-    <div class="body-content">
 
+    <div class="body-content">
+<!--
         <div class="row">
             <div class="col-lg-4">
                 <h2>Heading</h2>
@@ -46,6 +49,6 @@ $this->title = 'My Yii Application';
                 <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
             </div>
         </div>
-
+-->
     </div>
 </div>
